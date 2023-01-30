30e1ab3993336f1078b4663a965867a118a2b879
JohanDev <johanclipss@gmail.com>
2023-01-30 20:47:19 +0100

    Remove unnecessary comments: src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,4 @@ if (yargs.argv._[0] == null || yargs.argv._[0] == undefined) {
     console.log(Options.usage)
 } else {
     require(`./main/command/${yargs.argv._[0].toLowerCase()}.ts`).Execute(yargs.argv._[1])
-    /*if (fs.existsSync(`./main/command/${yargs.argv._[0]}.ts`)) {
-        require(`./main/command/${yargs.argv._[0].toLowerCase()}.ts`).Execute(yargs.argv._[1])
-    } else {
-        console.log(`brick: ${yargs.argv._[0].toLowerCase()}: Command not found.`)
-    }*/
 }
