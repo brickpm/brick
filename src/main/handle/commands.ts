f1758158011522169d3f67cf6d6f7a76ccd3e475
johron12 <71877077+johron12@users.noreply.github.com>
2023-01-31 09:00:33 +0000

    Creating modular command handler

---
+++ src/main/handle/commands.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    data: {
+        name: 'CommandHandler',
+        description: 'Handles commands',
+        author: 'Brick (JohainDev)',
+        version: '0.0.1'
+    },
+
+    async Execute(args: string[]) {
+        
+    }
+}
